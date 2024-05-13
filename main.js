50a47353c16ab98f6f29de00abc4389613caba88
StefanooV <99687887+StefanooV@users.noreply.github.com>
2024-05-13 13:35:17 -0300

    L2-end

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: '100% cotton stockings. The best quality on the market, very good for sports and better than any other'
+        }
+    }
+})
